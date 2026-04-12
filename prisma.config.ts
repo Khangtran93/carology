c5506b7063976b361f34bcf7fdd4427f68519b70
khangtranpt <khang.tran@prismtechinc.io>
2026-04-12 12:09:14 +1000

    fix prisma.config.ts

--- prisma.config.ts
+++ prisma.config.ts
@@ -2,8 +2,6 @@ import 'dotenv/config';
 import { defineConfig } from 'prisma/config';
 
 export default defineConfig({
-  schema: 'prisma/schema.prisma',
-  migrations: {
-    path: 'prisma/migrations',
-  },
+  earlyAccess: true,
+  schema: 'prisma/schema.prisma'
 });   
