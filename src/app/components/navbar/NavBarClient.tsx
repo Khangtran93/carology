5972d6dc650ccc27dca37dd468f52b038818e256
khangtranpt <khang.tran@prismtechinc.io>
2026-06-01 23:44:59 +1000

    fix car model UI on mobile

--- src/app/components/navbar/NavBarClient.tsx
+++ src/app/components/navbar/NavBarClient.tsx
@@ -5,29 +5,32 @@ import Link from 'next/link'
 import { logoFont } from '@/app/fonts/fonts'
 import { signOutAction } from '@/app/lib/auth'
 
-export default function NavbarClient({ isLoggedIn }: { isLoggedIn: boolean }) {
+export default function NavBarClient({ isLoggedIn }: { isLoggedIn: boolean }) {
   const [open, setOpen] = useState(false)
 
   return (
-    <div className={`sticky top-0 z-50 w-full bg-navy/90 backdrop-blur-md border-b border-slate-700/50 text-white ${logoFont.className}`}>
+    <div className={`sticky top-0 z-50 w-full bg-navy/90 backdrop-blur-md border border-slate-700/50 text-white ${logoFont.className}`}>
       <div className="flex justify-between items-center px-6 md:px-20 lg:px-40 py-4">
         <Link href="/" className="text-3xl">
           CAR<span className='text-red-500'>O</span>LOGY
         </Link>
-        <div  className="hidden md:flex items-center gap-x-6 text-2xl">
+        <div className="hidden md:flex items-center gap-x-6 text-2xl">
           {isLoggedIn ? (
             <>
               <Link href="/profile">Profile</Link>
-              <button className='p-0 cursor-pointer' type="submit" onClick={() => signOutAction()}>Sign Out</button>
+              <form action={signOutAction}>
+                <button className='p-0 cursor-pointer' type="submit">Sign Out</button>
+              </form>
             </>
           ) : (
-            <Link href="/login" onClick={() => setOpen(false)}>Login</Link>
+            <Link href="/login">Login</Link>
           )}
         </div>
 
         {/* Hamburger */}
         <button 
-          className={`${open && ""} md:hidden text-3xl`}
+          className={`md:hidden text-3xl`}
+          aria-label={open ? 'Close menu' : 'Open menu'}
           onClick={() => setOpen(prev => !prev)}
         >
           {open ? '✕' : '☰'}
@@ -44,8 +47,10 @@ export default function NavbarClient({ isLoggedIn }: { isLoggedIn: boolean }) {
           <div className="z-50 absolute top-full right-0 md:hidden flex flex-col gap-4 pt-4 px-6 pb-6 text-xl bg-navy/90 backdrop-blur-md border-b border-slate-700/50 rounded-bl-lg shadow-xl">
             {isLoggedIn ? (
               <>
-                <Link href="/profile">Profile</Link>
-                <button onClick={async () => { await signOutAction(); setOpen(false) }}>Sign Out</button>
+                <Link href="/profile" onClick={() => setOpen(false)}>Profile</Link>
+                <form action={signOutAction}>
+                  <button type="submit">Sign Out</button>
+                </form>
               </>
             ) : (
               <Link href="/login" onClick={() => setOpen(false)}>Login</Link>
